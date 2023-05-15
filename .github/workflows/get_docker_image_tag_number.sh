5f80fd3bb6e31b661f5e067bf0febc5268f5fd10
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-05-15 21:36:13 +0200

    .github/workflows: after app test pus Docker image to the Docker Hub repository

---
+++ .github/workflows/get_docker_image_tag_number.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# sudo apt-get update -y
+# sudo apt-get install -y --no-install-recommends --no-install-suggests \
+#      wget python3-minimal
+
+BASE_URL=https://registry.hub.docker.com/v2/repositories
+
+export DOCKERHUB_IMAGE_VERSION=$(wget -q \
+"${BASE_URL}/${{ secrets.DOCKERHUB_USERNAME }}/${{ secrets.DOCKERHUB_REPO_NAME }}/tags" -O - \
+| python3 -c \
+"""
+import sys, json
+results = json.load(sys.stdin)
+if not results['results']:
+   print(0)
+else:
+    print(int(results['results'][0]['name']) + 1)
+"""
+)
