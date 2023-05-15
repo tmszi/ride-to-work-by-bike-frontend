df2badba14053f0a272d58879f89472b97065bd4
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-05-15 22:48:58 +0200

    .github/workflows: pass args to get_docker_image_tag_number.sh script

--- .github/workflows/get_docker_image_tag_number.sh
+++ .github/workflows/get_docker_image_tag_number.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# sudo apt-get update -y
-# sudo apt-get install -y --no-install-recommends --no-install-suggests \
-#      wget python3-minimal
+DOCKERHUB_USERNAME=$1
+DOCKERHUB_REPO_NAME=$2
+
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends --no-install-suggests \
+     wget python3-minimal
 
 BASE_URL=https://registry.hub.docker.com/v2/repositories
 
 export DOCKERHUB_IMAGE_VERSION=$(wget -q \
-"${BASE_URL}/${{ secrets.DOCKERHUB_USERNAME }}/${{ secrets.DOCKERHUB_REPO_NAME }}/tags" -O - \
+"${BASE_URL}/${DOCKERHUB_USERNAME}/${DOCKERHUB_REPO_NAME}/tags" -O - \
 | python3 -c \
 """
 import sys, json
