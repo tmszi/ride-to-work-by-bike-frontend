ad11d6b823feeb50e89ad42cc72aa1f2fb338756
Šimon Macek <simon.macek@proficio.cz>
2023-06-10 16:13:18 +0200

    drawer menu test + fix

--- src/components/__tests__/VueDrawerMenu.cy.js
+++ src/components/__tests__/VueDrawerMenu.cy.js
@@ -1,9 +1,46 @@
 import VueDrawerMenu from '../VueDrawerMenu.vue';
+import { i18n } from '../../boot/i18n';
+
+const menuItems = [
+  { icon: 'home', name: 'home' },
+  { icon: 'route', name: 'routes' },
+  { icon: 'emoji_events', name: 'results' },
+  { icon: 'people', name: 'community' },
+  { icon: 'verified', name: 'discounts' },
+  { icon: 'business', name: 'coordinator' },
+  { icon: 'account_circle', name: 'profile' },
+  {
+    icon: 'email',
+    name: 'inviteFriends',
+  },
+  {
+    icon: 'volunteer_activism',
+    name: 'donate',
+  },
+];
 
 describe('VueDrawerMenu', () => {
   beforeEach(() => {
     cy.mount(VueDrawerMenu, {});
   });
+
+  it('has translation for all strings', () => {
+    const translationKeyList = menuItems.map((item) => `drawerMenu.${ item.name }`);
+
+    translationKeyList.forEach((translationKey) => {
+      const defaultEnglishString = i18n.global.t(translationKey, 'en');
+
+      const locales = i18n.global.availableLocales;
+      locales.filter((locale) => locale !== 'en').forEach((locale) => {
+        i18n.global.locale = locale;
+        const translatedString = i18n.global.t(translationKey);
+
+        cy.wrap(translatedString)
+          .should('be.a', 'string')
+          .and('not.equal', defaultEnglishString);
+      })
+    })
+  })
 
   it('should render the list with the correct number of items', () => {
     cy.window().then(() => {
@@ -12,24 +49,15 @@ describe('VueDrawerMenu', () => {
   });
 
   it('should render each item with the expected icon and text content', () => {
-    // TODO: Add dynamic translations tests
-    const items = [
-      { icon: 'home', name: 'Home' },
-      { icon: 'route', name: 'Routes' },
-      { icon: 'emoji_events', name: 'Results' },
-      { icon: 'people', name: 'Community' },
-      { icon: 'verified', name: 'Discounts' },
-      { icon: 'business', name: 'Coordinator' },
-      { icon: 'account_circle', name: 'Profile' },
-    ];
 
     cy.window().then(() => {
-      items.forEach((item, index) => {
+      menuItems.forEach((item, index) => {
         cy.get('.q-item')
           .eq(index)
           .within(() => {
-            cy.get('.q-icon').should('contain.text', item.icon);
-            cy.contains(item.name);
+            cy.get('.q-icon')
+              .should('be.visible')
+              .should('contain.text', item.icon);
           });
       });
     });
