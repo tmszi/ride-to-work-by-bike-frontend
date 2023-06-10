3cfaf0550bfb9835b9858f88b3fa6f854be42b86
Šimon Macek <simon.macek@proficio.cz>
2023-06-10 16:03:02 +0200

    layout: edit drawer width

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,7 +30,6 @@ export default defineComponent({
       show-if-above
       v-model="leftDrawerOpen"
       side="left"
-      :width="360"
       class="bg-gray-light q-py-lg q-px-lg"
     >
       <vue-drawer-header></vue-drawer-header>
