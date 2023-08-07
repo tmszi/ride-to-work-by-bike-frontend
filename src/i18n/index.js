5b1568bb307ac15c364b1dd953fb941185cd139f
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-08-07 20:49:55 +0200

    src/i18n: import locales modules directly

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,5 @@
 export const loadLocaleMessages = () => {
-  const locales = import.meta.glob('./[A-Za-z0-9-_,s]+.toml');
+  const locales = import.meta.glob('./[A-Za-z0-9-_,s]+.toml', { eager: true });
   let messages = {};
   Object.keys(locales).forEach((key) => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
