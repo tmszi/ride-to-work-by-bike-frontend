2abe3949100bec2cc523e96fe205648ab071c829
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-08-15 14:27:17 +0200

    src/i18n: remove duplicates from regex character class

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@ export const loadLocaleMessages = () => {
   const locales = import.meta.glob('./[A-Za-z0-9-_,s]+.toml', { eager: true });
   let messages = {};
   Object.keys(locales).forEach((key) => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    const matched = key.match(/([a-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
       // Dynamic import TOML translation file content
