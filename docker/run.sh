19459321d10048605953eb15f9815d0b1fafa5ef
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-10-31 07:34:18 +0100

    Fix run production Docker container

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#
+# Build and copy app content into web server dir
 
-quasar build && cp -R dist/spa/* /var/www/html && chown nginx:nginx /var/www/html
+
+yarn build && cp -R dist/spa/* /var/www/html && chown nginx:nginx /var/www/html
 # Start nginx and keep the process from backgrounding and the container from quitting
 nginx -g "daemon off;"
