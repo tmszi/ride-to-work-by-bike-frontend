baeaab0ce8ed08c1b18a8b79fb819c42db98b480
Simon Macek <42778183+maceksimon@users.noreply.github.com>
2024-05-10 04:43:28 +0200

    src/components/__tests__: simplify ListCardPost tests by removing cy.window wrapper (#376)
    * list-card-post: simplify test
    
    * list-card-post replace hex color in the tests
    
    ---------
    
    Co-authored-by: Šimon Macek <simon.macek@proficio.cz>

--- src/components/__tests__/ListCardPost.cy.js
+++ src/components/__tests__/ListCardPost.cy.js
@@ -14,6 +14,7 @@ const button = {
 
 const { getPaletteColor } = colors;
 const black = getPaletteColor('black');
+const gray10 = getPaletteColor('grey-10');
 
 describe('<ListCardPost>', () => {
   it('has translation for all strings', () => {
@@ -36,97 +37,58 @@ describe('<ListCardPost>', () => {
       cy.viewport('macbook-16');
     });
 
-    it('renders title', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-title')
-          .should('have.css', 'font-size', '20px')
-          .and('have.css', 'font-weight', '500')
-          .and('have.color', black)
-          .and('contain', title)
-          .then(($title) => {
-            expect($title.text()).to.equal(title);
-          });
-      });
-    });
-
-    it('renders correct number of items', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-item').should('have.length', 5);
-      });
-    });
+    coreTests();
 
     it('renders swiper navigation buttons', () => {
-      cy.window().then(() => {
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-prev')
-          .should('be.visible')
-          .and('have.css', 'width', '38px')
-          .and('have.css', 'height', '38px')
-          .and('have.css', 'border', `1px solid ${hexToRgb('#212121')}`);
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-next')
-          .should('be.visible')
-          .and('have.css', 'width', '38px')
-          .and('have.css', 'height', '38px')
-          .and('have.css', 'border', `1px solid ${hexToRgb('#212121')}`);
-      });
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-prev')
+        .should('be.visible')
+        .and('have.css', 'width', '38px')
+        .and('have.css', 'height', '38px')
+        .and('have.css', 'border', `1px solid ${hexToRgb(gray10)}`);
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-next')
+        .should('be.visible')
+        .and('have.css', 'width', '38px')
+        .and('have.css', 'height', '38px')
+        .and('have.css', 'border', `1px solid ${hexToRgb(gray10)}`);
     });
 
     it('changes button disabled state after navigation', () => {
-      cy.window().then(() => {
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-prev')
-          .should('have.css', 'opacity', '0.35');
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-next')
-          .should('have.css', 'opacity', '1');
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-next')
-          .click();
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-prev')
-          .should('have.css', 'opacity', '1');
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-next')
-          .should('have.css', 'opacity', '0.35');
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-prev')
-          .click();
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-prev')
-          .should('have.css', 'opacity', '0.35');
-        cy.dataCy('swiper-container')
-          .shadow()
-          .find('.swiper-button-next')
-          .should('have.css', 'opacity', '1');
-      });
-    });
-
-    it('renders button container', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-buttons')
-          .should('be.visible')
-          .and('have.css', 'text-align', 'center');
-      });
-    });
-
-    it('renders button', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-button')
-          .should('be.visible')
-          .and('have.css', 'border-color', hexToRgb('#212121'))
-          .and('have.css', 'border-radius', '28px')
-          .and('contain', button.title);
-      });
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-prev')
+        .should('have.css', 'opacity', '0.35');
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-next')
+        .should('have.css', 'opacity', '1');
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-next')
+        .click();
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-prev')
+        .should('have.css', 'opacity', '1');
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-next')
+        .should('have.css', 'opacity', '0.35');
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-prev')
+        .click();
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-prev')
+        .should('have.css', 'opacity', '0.35');
+      cy.dataCy('swiper-container')
+        .shadow()
+        .find('.swiper-button-next')
+        .should('have.css', 'opacity', '1');
     });
   });
 
@@ -142,41 +104,51 @@ describe('<ListCardPost>', () => {
       cy.viewport('iphone-6');
     });
 
-    it('renders title', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-title')
-          .should('have.css', 'font-size', '20px')
-          .and('have.css', 'font-weight', '500')
-          .and('have.color', black)
-          .and('contain', title)
-          .then(($title) => {
-            expect($title.text()).to.equal(title);
-          });
-      });
+    coreTests();
+
+    it('renders full width button container', () => {
+      cy.dataCy('card-list-post-buttons').should('be.visible');
+      cy.testElementPercentageWidth(cy.dataCy('card-list-post-buttons'), 100);
     });
 
-    it('renders correct number of items', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-item').should('have.length', 5);
-      });
-    });
-
-    it('renders button container', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-buttons').should('be.visible');
-        cy.testElementPercentageWidth(cy.dataCy('card-list-post-buttons'), 100);
-      });
-    });
-
-    it('renders button', () => {
-      cy.window().then(() => {
-        cy.dataCy('card-list-post-button')
-          .should('be.visible')
-          .and('have.css', 'border-color', hexToRgb('#212121'))
-          .and('have.css', 'border-radius', '28px')
-          .and('contain', button.title);
-        cy.testElementPercentageWidth(cy.dataCy('card-list-post-button'), 100);
-      });
+    it('renders full width button', () => {
+      cy.dataCy('card-list-post-button')
+        .should('be.visible')
+        .and('have.css', 'border-color', hexToRgb(gray10))
+        .and('have.css', 'border-radius', '28px')
+        .and('contain', button.title);
+      cy.testElementPercentageWidth(cy.dataCy('card-list-post-button'), 100);
     });
   });
 });
+
+function coreTests() {
+  it('renders title', () => {
+    cy.dataCy('card-list-post-title')
+      .should('have.css', 'font-size', '20px')
+      .and('have.css', 'font-weight', '500')
+      .and('have.color', black)
+      .and('contain', title)
+      .then(($title) => {
+        expect($title.text()).to.equal(title);
+      });
+  });
+
+  it('renders correct number of items', () => {
+    cy.dataCy('card-list-post-item').should('have.length', 5);
+  });
+
+  it('renders button container', () => {
+    cy.dataCy('card-list-post-buttons')
+      .should('be.visible')
+      .and('have.css', 'text-align', 'center');
+  });
+
+  it('renders button', () => {
+    cy.dataCy('card-list-post-button')
+      .should('be.visible')
+      .and('have.css', 'border-color', hexToRgb(gray10))
+      .and('have.css', 'border-radius', '28px')
+      .and('contain', button.title);
+  });
+}
