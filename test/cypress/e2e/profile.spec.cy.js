44e19a345c161ed3fd7220dd69f49ad71da78de2
Simon Macek <42778183+maceksimon@users.noreply.github.com>
2024-09-23 21:26:41 +0200

    fix visibility test for drawer menu (#583)
    Co-authored-by: Šimon Macek <simon.macek@proficio.cz>

--- test/cypress/e2e/profile.spec.cy.js
+++ test/cypress/e2e/profile.spec.cy.js
@@ -47,7 +47,8 @@ describe('Profile page', () => {
       cy.dataCy('drawer-header').should('be.visible');
       cy.dataCy('user-select').should('be.visible');
       cy.dataCy('drawer-toggle-buttons').should('be.visible');
-      cy.dataCy('drawer-menu').should('be.visible');
+      cy.dataCy('drawer-menu-top').should('be.visible');
+      cy.dataCy('drawer-menu-bottom').should('be.visible');
     });
   });
 });
