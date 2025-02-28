f94b3aeaf91f842d648bfd418d27f606496a29da
Simon Macek <42778183+maceksimon@users.noreply.github.com>
2025-02-28 09:42:50 +0100

    test/cypress/support: fix 400 response code when testing external links (Twitter) (#926)
    * allow 400 response code when testing external links
    
    * provide exception for twitter URL
    
    * update comment
    
    * update headers for programmatic test requests and revert other changes
    
    * cleanup
    
    ---------
    
    Co-authored-by: Šimon Macek <simon.macek@proficio.cz>

--- test/cypress/support/commonTests.ts
+++ test/cypress/support/commonTests.ts
@@ -633,7 +633,8 @@ export const httpTooManyRequestsStatusMessage = `HTTP status code ${httpTooManyR
 export const failOnStatusCode = false;
 export const userAgentHeader = {
   'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+  'Sec-Fetch-Mode': 'navigate',
 };
 export const interceptedRequestResponseDelay = 300;
 
