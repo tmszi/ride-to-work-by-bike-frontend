462d2db1732f57f03b072a61e6aed571ba17081a
Tomas Zigo <50632337+tmszi@users.noreply.github.com>
2026-03-22 07:35:19 +0100

    src/pages: add missing PrivacyPage component (#1365)

---
+++ src/pages/PrivacyPage.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+/**
+ * PrivacyPage Component
+ *
+ * The `PrivacyPage` renders the page with privacy policy rules.
+ *
+ * @description
+ * This component is used to display privacy policy rules.
+ *
+ * @components
+ * - `PrivacyRules`: Component to render privacy policy rules.
+ * - `LoginRegisterHeader`: Component to render page header.
+ *
+ * @layout
+ * - `LoginRegisterLayout`: Displayed in the `LoginRegisterLayout` template.
+ *
+ */
+
+// libraries
+import { defineComponent } from 'vue';
+
+// components
+import PrivacyRules from 'components/global/PrivacyRules.vue';
+import LoginRegisterHeader from 'components/global/LoginRegisterHeader.vue';
+
+export default defineComponent({
+  name: 'PrivacyPage',
+  components: {
+    LoginRegisterHeader,
+    PrivacyRules,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <q-page padding>
+    <div class="q-px-lg">
+      <!-- Page header -->
+      <login-register-header data-cy="login-register-header" />
+
+      <div class="row q-mt-xl">
+        <div class="col-12 col-md-4">
+          <privacy-rules />
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
